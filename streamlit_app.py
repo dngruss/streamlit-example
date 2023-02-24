665a0597ac03dc18f87779e44b15751722247644
Wade Russ <wruss@pandologic.com>
2023-02-24 17:28:35 -0500

    More stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,12 @@ This is a new thing
 """
 
 with st.echo(code_location='below'):
-    thinga = st.slider("Age", 1, 100, 46)
+    import streamlit as st
+    import pandas as pd
+    import numpy as np
+
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+
+    st.area_chart(chart_data)
